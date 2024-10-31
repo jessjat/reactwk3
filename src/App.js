bdc4af48b765fcb29a106a3d4837ddb6774f36cc
jessjat <129844465+jessjat@users.noreply.github.com>
2024-10-31 13:53:20 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Weather from "./Weather";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <Weather />
+    </div>
+  );
+}
